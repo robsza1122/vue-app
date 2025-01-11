cde83eef4bbdceb7a8d8751b74cd94a3c7b93030
Robert <robert.szalapak@gmail.com>
2025-01-11 17:39:22 +0100

    deployment with yml file is ready

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,5 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="root"><div class="todoapp"><h1 class="todoapp__title">todos</h1><div class="todoapp__content"><header class="todoapp__header"><button type="button" class="todoapp__toggle-all" data-cy="ToggleAllButton"></button><form><input data-cy="NewTodoField" type="text" class="todoapp__new-todo" placeholder="What needs to be done?" value=""></form></header><section class="todoapp__main" data-cy="TodoList"><div data-cy="Todo" class="todo"><label class="todo__status-label"><input data-cy="TodoStatus" type="checkbox" class="todo__status"></label><span data-cy="TodoTitle" class="todo__title">iii</span><button type="button" class="todo__remove" data-cy="TodoDelete">×</button><div data-cy="TodoLoader" class="modal overlay"><div class="modal-background has-background-white-ter"></div><div class="loader"></div></div></div><div data-cy="Todo" class="todo"><label class="todo__status-label"><input data-cy="TodoStatus" type="checkbox" class="todo__status"></label><span data-cy="TodoTitle" class="todo__title">yyy</span><button type="button" class="todo__remove" data-cy="TodoDelete">×</button><div data-cy="TodoLoader" class="modal overlay"><div class="modal-background has-background-white-ter"></div><div class="loader"></div></div></div><div data-cy="Todo" class="todo"><label class="todo__status-label"><input data-cy="TodoStatus" type="checkbox" class="todo__status"></label><span data-cy="TodoTitle" class="todo__title">ppp</span><button type="button" class="todo__remove" data-cy="TodoDelete">×</button><div data-cy="TodoLoader" class="modal overlay"><div class="modal-background has-background-white-ter"></div><div class="loader"></div></div></div></section><footer class="todoapp__footer" data-cy="Footer"><span class="todo-count" data-cy="TodosCounter">3 items left</span><nav class="filter" data-cy="Filter"><a href="#/" class="filter__link selected" data-cy="FilterLinkAll">All</a><a href="#/active" class="filter__link" data-cy="FilterLinkActive">Active</a><a href="#/completed" class="filter__link" data-cy="FilterLinkCompleted">Completed</a></nav><button type="button" class="todoapp__clear-completed" data-cy="ClearCompletedButton" disabled>Clear completed</button></footer></div><div data-cy="ErrorNotification" class="notification is-danger is-light has-text-weight-normal hidden"><button data-cy="HideErrorButton" type="button" class="delete"></button></div></div></div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
